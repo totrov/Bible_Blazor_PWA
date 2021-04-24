03a639f14580bb3fe61027f72e16ee8564c13725
Oleg Totrov <totrov.ov@gmail.com>
2021-04-24 21:38:04 +0300

    save lesson component started

--- LessonContainer.cs
+++ LessonContainer.cs
@@ -1,8 +1,12 @@
 ﻿namespace Bible_Blazer_PWA
 {
-    public class Lesson
+    public class LessonContainer
     {
         public string LinkName { get; set; }
         public string Content { get; set; }
+        public string[] getLines()
+        {
+            return Content.Split("<br>");
+        }
     }
 }
