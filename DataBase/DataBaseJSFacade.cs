419924ea09c0fff869199b9afabc0b6f5c90236e
Oleg Totrov <totrov.ov@gmail.com>
2021-05-09 01:44:51 +0300

    db started. Preliminary save

---
+++ DataBase/DataBaseJSFacade.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bible_Blazer_PWA.DataBase
+{
+    public class DatabaseJSFacade
+    {
+        protected IJSRuntime JS { get; set; }
+        internal bool Result
+        {
+            get
+            {
+                return _isInitialized;
+            }
+            set {
+                _isInitialized = value;
+                JS.InvokeVoidAsync("database.jsLog", $"set {value} value");
+            }
+        }
+        internal bool _isInitialized { get; set; }
+        private IndexedDBResultHandler resultHandler;
+        private DotNetObjectReference<IndexedDBResultHandler> resultHandlerReference;
+
+        public async Task<string> Test()
+        {
+            return await JS.InvokeAsync<string>("database.showPrompt");
+        }
+
+        public bool IsInitialized { get { return _isInitialized; } }
+
+        public async Task Init(IJSRuntime js)
+        {
+            JS = js;
+            resultHandler = new IndexedDBResultHandler(this);
+            resultHandlerReference = DotNetObjectReference.Create(resultHandler);
+            await JS.InvokeVoidAsync("database.initDatabase", resultHandlerReference);
+        }
+    }
+
+    public class IndexedDBResultHandler
+    {
+        private DatabaseJSFacade db;
+        public IndexedDBResultHandler(DatabaseJSFacade _db)
+        {
+            db = _db;
+        }
+
+        [JSInvokable]
+        public void SetResult(bool _result)
+        {
+            db.Result = _result;
+            if (_result)
+            {
+                db._isInitialized = true;
+            }
+        }
+    }
+}
--- wwwroot/js/database.js
+++ wwwroot/js/database.js
@@ -1,42 +1,110 @@
-﻿function initDatabase(books) {
+﻿window.context = {
+    db: null,
+    justUpgraded: false
+};
 
-    let openRequest = indexedDB.open("store", 1);
+function DataUpgrade(dotnetReference)
+{
+    console.log("Data upgrade started");
+    console.log("dbVersion:" + context.db.version);
+    
+    switch (context.db.version)
+    {
+        case 1:
+            let book = { id: 'js', price: 10 };
 
-    openRequest.onerror = function () {
-        console.error("Error", openRequest.error);
-    };
+            let transaction = context.db.transaction("books", "readwrite");
+            transaction.objectStore("books").add(book);
 
-    openRequest.onsuccess = function () {
-        let book = { id: 'js', price: 10 };
+            transaction.oncomplete = function () {
+                console.log("Транзакция выполнена");
+                dotnetReference.invokeMethod('SetResult', true);
+            };
 
-        let db = openRequest.result;
-        let transaction = db.transaction("books", "readwrite");
-        let request = transaction.objectStore("books").add(book);
+            transaction.onerror = function (e) {
+                console.log("Транзакция не выполнена." + e.error);
+                dotnetReference.invokeMethod('SetResult', false);
+                e.stopPropagation();
+            };
+            break;
 
-        transaction.oncomplete = function () {
-            console.log("Транзакция выполнена");
-        };
-    };
-
-    openRequest.onupgradeneeded = function () {
-        // версия существующей базы данных меньше 2 (или база данных не существует)
-        let db = openRequest.result;
-        switch (db.version) { // существующая (старая) версия базы данных
-            case 0:
-                db.createObjectStore('books', { keyPath: 'id' });
-                //db.createObjectStore('verses', { keyPath: 'id' });
-                break;
-            case 1:
-                // на клиенте версия базы данных 1
-                // обновить
-                break;
-            default:
-                break;
-
-        }
+        default:
+            console.log("no data upgrade script for current db version");
+            break;
     }
 }
 
-function getVerseById(dotnetCallback, bookId, verseId) {
-    let result = "Bible verse stub: " + bookId + ":" + verseId;
+function SchemaUpgrade() {
+
+    console.log("Schema upgrade started");
+    console.log("dbVersion:" + context.db.version);
+    let upgradeWasNotSuccess = false;
+
+    switch (context.db.version) {
+        case 1:
+            context.db.createObjectStore('books', { keyPath: 'id' });
+            break;
+
+        default:
+            upgradeWasNotSuccess = true;
+            console.log("no schema upgrade script for current db version");
+            break;
+    }
+
+    if (!upgradeWasNotSuccess) {
+        context.justUpgraded = true;
+    }
 }
+
+
+window.database = {
+
+    initDatabase: function (dotnetReference) {
+        console.log("Database initialization started");
+
+        //indexedDB.deleteDatabase("store");
+        let openRequest = indexedDB.open("db", 1);
+
+        openRequest.onerror = function () {
+            console.error("Error", openRequest.error);
+            dotnetReference.invokeMethod('SetResult', false);
+        };
+
+        openRequest.onsuccess = function () {
+            context.db = openRequest.result;
+            if (context.justUpgraded) {
+                DataUpgrade(dotnetReference);
+            }
+            else {
+                dotnetReference.invokeMethod('SetResult', true);
+            }
+            console.log("Database initialization finished");
+        };
+
+        openRequest.onupgradeneeded = function () {
+            context.db = openRequest.result;
+            SchemaUpgrade();
+        }
+    },
+    getVerseById: function (dotnetCallback, bookId, verseId) {
+        let result = "Bible verse stub: " + bookId + ":" + verseId;
+    },
+    displayWelcome: function (welcomeMessage) {
+        document.getElementById('welcome').innerText = welcomeMessage;
+    },
+    returnArrayAsyncJs: function () {
+        DotNet.invokeMethodAsync('BlazorWebAssemblySample', 'ReturnArrayAsync')
+            .then(data => {
+                data.push(4);
+                console.log(data);
+            });
+    },
+    jsLog: function (text) { console.log(text);},
+    showPrompt: function (text) {
+        return prompt(text, 'Type your name here');
+    },
+    sayHello: function (dotnetHelper) {
+        return dotnetHelper.invokeMethodAsync('SayHello')
+            .then(r => console.log(r));
+    }
+};
