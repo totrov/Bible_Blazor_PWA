f9c2e073bff00d3ad465dac1654c3cb5b8171955
Oleg Totrov <totrov.ov@gmail.com>
2021-05-23 03:41:59 +0300

    !not working:: try to implement asynchronous elements in componentr=

---
+++ IBibleServiceFetchStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using static Bible_Blazer_PWA.BibleService;
+
+namespace Bible_Blazer_PWA
+{
+    public interface IBibleServiceFetchStrategy
+    {
+        Task<IEnumerable<Verse>> GetVersesAsync(int bookId, int chapter, int fromVerse, int? toVerse);
+        Task<int> GetBookIdByShortName(string bookShortName);
+    }
+}
