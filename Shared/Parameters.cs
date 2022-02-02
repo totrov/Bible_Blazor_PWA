577d6e69946ecbcf11597a8683129d03ea425ff2
Oleg Totrov <totrov.ov@gmail.com>
2022-02-02 21:00:06 +0300

    Floating collaps buttons + bottom navigation + 2nd lesson replacements

---
+++ Shared/Parameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bible_Blazer_PWA.Shared
+{
+    public class Parameters
+    {
+        public bool AreReferencesOpened { get; set; }
+        public byte CollapseLevel { get; set; }
+    }
+}
