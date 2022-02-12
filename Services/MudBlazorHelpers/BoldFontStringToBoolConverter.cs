690f36cee8117748cd7818b42359ac4bfaab225a
Oleg Totrov <totrov.ov@gmail.com>
2022-02-12 23:24:02 +0300

    MudBlazor on Setup

---
+++ Services/MudBlazorHelpers/BoldFontStringToBoolConverter.cs
@@ -0,0 +1,54 @@
+﻿using MudBlazor;
+using System;
+
+namespace Bible_Blazer_PWA.Services.MudBlazorHelpers
+{
+    public class BoldFontStringToBoolConverter : BoolConverter<string>
+    {
+
+        public BoldFontStringToBoolConverter()
+        {
+            SetFunc = OnSet;
+            GetFunc = OnGet;
+        }
+
+        private string TrueString = "bold";
+        private string FalseString = "normal";
+        private string NullString = "normal";
+
+        private string OnGet(bool? value)
+        {
+            try
+            {
+                return (value == true) ? TrueString : FalseString;
+            }
+            catch (Exception e)
+            {
+                UpdateGetError("Conversion error: " + e.Message);
+                return NullString;
+            }
+        }
+
+        private bool? OnSet(string arg)
+        {
+            if (arg == null)
+                return null;
+            try
+            {
+                if (arg == TrueString)
+                    return true;
+                if (arg == FalseString)
+                    return false;
+                else
+                    return null;
+            }
+            catch (FormatException e)
+            {
+                UpdateSetError("Conversion error: " + e.Message);
+                return null;
+            }
+        }
+
+    }
+}
+
