8c6f8a36cda632eb218ff07dd82471373437ef82
Oleg Totrov <totrov.ov@gmail.com>
2022-03-21 03:06:33 +0300

    temporary removing YoutubeExplode due to pipeline failure

--- YoutubeExplode/Utils/Extensions/GenericExtensions.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace YoutubeExplode.Utils.Extensions;
-
-internal static class GenericExtensions
-{
-    public static TOut Pipe<TIn, TOut>(this TIn input, Func<TIn, TOut> transform) => transform(input);
-}
