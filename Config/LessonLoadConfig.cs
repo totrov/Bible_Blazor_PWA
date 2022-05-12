787c8465e9a21935ae35e36d47fb52310070490d
Oleg Totrov <totrov.ov@gmail.com>
2022-05-12 03:05:42 +0300

    Loading redesign started

---
+++ Config/LessonLoadConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Bible_Blazer_PWA.Config
+{
+    public class LessonLoadConfig
+    {
+        private static Dictionary<string, string> lessonNameUrl = new()
+        {
+            {"Бытие","" },
+            {"Исход - Соломон","" },
+            {"Пророки","" },
+            {"Евангелия","" },
+            {"Деяния - Откровение","" },
+            {"Основы веры","" }
+        };
+
+        public static IEnumerable<string> GetLessonNames()
+        {
+            return lessonNameUrl.Keys;
+        }
+
+        public static string GetUrlByLessonName(string lessonName)
+        {
+            return lessonNameUrl[lessonName];
+        }
+    }
+}
