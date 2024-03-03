9e5b5383b61e0eecc7b8b6a85188dcbf240c3c6e
Oleg Totrov <totrov.ov@gmail.com>
2024-03-03 02:35:24 +0100

    [RELEASE v2.9] paddings fix and scrolling in bottom mode fix

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -1,6 +1,6 @@
 ﻿// Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
-// version - 2.8
+// version - 2.9
 
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
