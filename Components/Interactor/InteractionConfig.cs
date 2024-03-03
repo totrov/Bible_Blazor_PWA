e16c20ecfc7e3bdb318b0bcdbc4d86a2011faa0f
Oleg Totrov <totrov.ov@gmail.com>
2024-03-03 01:19:31 +0100

    panel content scrolling fix for bottom mode

--- Components/Interactor/InteractionConfig.cs
+++ Components/Interactor/InteractionConfig.cs
@@ -4,18 +4,18 @@ namespace Bible_Blazer_PWA.Components.Interactor
 {
     public class InteractionConfig
     {
-        public Dictionary<int, string[]> SizeMap { get; init; }
+        public Dictionary<int, decimal[]> SizeMap { get; init; }
         public InteractionConfig()
         {
             SizeMap = new()
             {
-                { 2, new []{ $"20%", $"80%" } },
-                { 3, new []{ $"13.3333333%", $"86.666667%" } },
-                { 4, new []{ $"10%", $"90%" } },
-                { 5, new []{ $"8%", $"92%" } },
-                { 6, new []{ $"6.66666667%", $"93.3333333%" } },
-                { 7, new []{ $"5.71428571%", $"94.28571429%" } },
-                { 8, new []{ $"5%" , $"95%" } }
+                { 2, new []{ 20M, 80M } },
+                { 3, new []{ 13.3333333M, 86.666667M } },
+                { 4, new []{ 10M, 90M } },
+                { 5, new []{ 8M, 92M } },
+                { 6, new []{ 6.66666667M, 93.3333333M } },
+                { 7, new []{ 5.71428571M, 94.28571429M } },
+                { 8, new []{ 5M, 95M } }
             };
         }
     }
