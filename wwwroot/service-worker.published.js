8333cf9ec1d4deda2f68a639c7d7b6d3267be8d4
totrov <totrov.ov@gmail.com>
2026-06-28 23:29:33 +0200

    Locked Lessons under password

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -1,6 +1,6 @@
 ﻿// Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
-// version - v2.25
+// version - v2.26 (locked)
 
 self.importScripts('./service-worker-assets.js');
 self.addEventListener('install', event => event.waitUntil(onInstall(event)));
